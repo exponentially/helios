997f5c1479f1515f19ace3d9682bf0209aca6fdd
Milan Jaric <milan.jaric@gmail.com>
2019-07-05 13:15:07 +0200

    adapter typespec improvement

--- lib/helios/pipeline/adapter.ex
+++ lib/helios/pipeline/adapter.ex
@@ -10,7 +10,7 @@ defmodule Helios.Pipeline.Adapter do
   @type status :: Context.status()
   @type response :: Context.response()
 
-  @type sent_body :: binary | nil
+  @type sent_body :: term | nil
 
   @callback send_resp(payload, status, response) :: {:ok, sent_body, payload}
 end
